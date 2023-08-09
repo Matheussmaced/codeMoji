886be1b7532cd23f4340e14bd26f60cbf1ff9ecc
Matheussmaced <mateusbyakuya3@gmail.com>
2023-08-09 12:06:40 -0300

    update style

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+const Body = () => {
+    return(
+        <div id="containerBody">
+            <div id="informs">
+                <p>Apple</p>
+                <h1>Melhor site para criar memoji. Fácil, simples e rápido.</h1>
+
+                <div id="buttons">
+                    <button>criar memoji</button>
+                    <button>ver biblioteca</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body
