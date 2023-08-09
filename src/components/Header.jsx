f23a54a3a91aae4ab5ce6464326a14da3dd88458
Matheussmaced <mateusbyakuya3@gmail.com>
2023-08-09 11:53:00 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+//Import imgs
+import search from '../assets/search.svg' 
+
+const Header = () => {
+    return(
+        <div id="containerHeader">
+            <header>
+                <div id="logo">
+                    <h2>CodeMoji</h2>
+                </div>
+                <div id="searchAndUserLogin">
+                    <div id="search">
+                        <img src={search} alt="Search icon" />
+                    </div>
+                    <div id="loginUser">
+                        <h3>Fazer login</h3>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
